92444055db3a45f40c490e1581710e517149d750
Grey <sergejyurskyj@yahoo.com>
2020-02-10 20:49:29 -0800

    Committed Section 3 ClassesThe Blueprints of Object Oriented Programming

---
+++ Section 4 Instances and Instance Attributes/Mini-Project-Bakery-System-Fix-the-Bugs.py
@@ -0,0 +1,20 @@
+class Donut
+        def _ini__(flavor, toppings, filling, size):
+                self.flavor = flavor
+                self.toppings = toppings
+                self.filling = filling
+                size = self.size
+
+class Customer:
+
+	def _init_(self, name, age address, favorite_dessert)
+		self.name = name
+		self.age = age
+		self.address = address
+		favorite_dessert = self.favorite_dessert
+
+Cake:
+	__init__(self, flavor, price, quality):
+		self.flavor = flavor
+		self.price = price
+		self.quality = quality
